56ed78dd8bf14bf57d6e358b004ca86d7771f4f9
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-31 17:04:47 -0500

    Add GET /todos route.

--- server/server.js
+++ server/server.js
@@ -24,6 +24,13 @@ app.post('/todos', (req, res) => {
 });
 
 // Get todos.
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000.');
