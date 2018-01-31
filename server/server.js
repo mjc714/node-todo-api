d55058779e882a274f2ed3ec342c021f70ed7b92
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-31 15:41:29 -0500

    Create POST todos route.
    Configured mongoose.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,23 @@ const { User } = require('./models/user');
 
 const app = express();
 
+app.use(bodyParser.json());
 
+// POST: Create new todos.
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+
+});
+
+// Get todos.
 
 app.listen(3000, () => {
     console.log('Started on port 3000.');
